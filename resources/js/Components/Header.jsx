47a558011b164d86be541f0cee9957653d99205e
victormoreiraruiz <victormanuel.moreira@iesdonana.org>
2024-10-30 13:59:11 +0100

    intentando que se vea (no funciona)

---
+++ resources/js/Components/Header.jsx
@@ -0,0 +1,18 @@
+// Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="menu">
+        <a href="construccion.html">GALERIA</a>
+        <a href="construccion.html">EVENTOS</a>
+        <a href="construccion.html">ENTRADAS</a>
+        <a href="nosotros.html">NOSOTROS</a>
+        <a href="contactanos.html">CONTACTANOS</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
