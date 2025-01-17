6f638d1fac37d90cef32c7bccc4ce7dfd4f742fe
victormoreiraruiz <victormanuel.moreira@iesdonana.org>
2025-01-17 13:43:22 +0100

    formulario promotor y reservas

--- resources/js/Components/EventosSalaCelebraciones.jsx
+++ resources/js/Components/EventosSalaCelebraciones.jsx
@@ -4,12 +4,9 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 import '../../css/app.css'; // Asegura que este CSS se carga después del de react-calendar
 
-
-
 const EventosSalaCelebraciones = () => {
   const [motivo, setMotivo] = useState('');
   const [numeroPersonas, setNumeroPersonas] = useState(40);
-  const [tipoReserva, setTipoReserva] = useState('privada');
   const [precioEntrada, setPrecioEntrada] = useState('');
   const [nombreConcierto, setNombreConcierto] = useState('');
   const [horaInicio, setHoraInicio] = useState('');
@@ -20,6 +17,10 @@ const EventosSalaCelebraciones = () => {
   const [acceptPolicies, setAcceptPolicies] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    fetchBookedDates();
+  }, []);
+
   const fetchBookedDates = async () => {
     try {
       const response = await axios.get('/api/salas/2/reservas');
@@ -28,10 +29,6 @@ const EventosSalaCelebraciones = () => {
       console.error('Error al cargar las fechas de reservas:', error);
     }
   };
-
-  useEffect(() => {
-    fetchBookedDates();
-  }, []);
 
   const handleDateChange = (date) => {
     if (!isDateBooked(date)) {
@@ -47,6 +44,29 @@ const EventosSalaCelebraciones = () => {
 
   const handleCartelChange = (e) => {
     setCartel(e.target.files[0]);
+  };
+
+  const validateTimes = () => {
+    const [horaInicioH, horaInicioM] = horaInicio.split(":").map(Number);
+    const [horaFinH, horaFinM] = horaFin.split(":").map(Number);
+
+    if (horaInicioH < 14) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Hora de inicio inválida',
+        text: 'La hora de inicio no puede ser antes de las 14:00.',
+      });
+      return false;
+    }
+    if (horaFinH > 7 || (horaFinH === 7 && horaFinM > 0)) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Hora de fin inválida',
+        text: 'La hora de fin no puede ser más tarde de las 07:00.',
+      });
+      return false;
+    }
+    return true;
   };
 
   const handleSubmit = async (e) => {
@@ -70,16 +90,17 @@ const EventosSalaCelebraciones = () => {
       return;
     }
 
-    if (
-      tipoReserva === 'concierto' &&
-      (!precioEntrada || !nombreConcierto || !horaInicio || !horaFin)
-    ) {
+    if (!precioEntrada || !nombreConcierto || !horaInicio || !horaFin) {
       Swal.fire({
         icon: 'warning',
         title: 'Campos obligatorios',
         text: 'Por favor, completa todos los campos obligatorios para el concierto.',
       });
       return;
+    }
+
+    if (!validateTimes()) {
+      return; // Si la validación de horas falla, no continúa
     }
 
     const adjustedDate = new Date(selectedDate.getTime() - selectedDate.getTimezoneOffset() * 60000)
@@ -90,11 +111,11 @@ const EventosSalaCelebraciones = () => {
     formData.append('fecha_reserva', adjustedDate);
     formData.append('descripcion', motivo);
     formData.append('asistentes', numeroPersonas);
-    formData.append('tipo_reserva', tipoReserva);
-    formData.append('precio_entrada', tipoReserva === 'concierto' ? precioEntrada : null);
-    formData.append('nombre_concierto', tipoReserva === 'concierto' ? nombreConcierto : '');
-    formData.append('hora_inicio', tipoReserva === 'concierto' ? horaInicio : '');
-    formData.append('hora_fin', tipoReserva === 'concierto' ? horaFin : '');
+    formData.append('tipo_reserva', 'concierto');
+    formData.append('precio_entrada', precioEntrada);
+    formData.append('nombre_concierto', nombreConcierto);
+    formData.append('hora_inicio', horaInicio);
+    formData.append('hora_fin', horaFin);
     if (cartel) {
       formData.append('cartel', cartel);
     }
@@ -113,7 +134,6 @@ const EventosSalaCelebraciones = () => {
 
       setMotivo('');
       setNumeroPersonas(40);
-      setTipoReserva('privada');
       setPrecioEntrada('');
       setNombreConcierto('');
       setHoraInicio('');
@@ -123,50 +143,36 @@ const EventosSalaCelebraciones = () => {
       setAcceptPolicies(false);
       fetchBookedDates();
     } catch (error) {
-      if (error.response && error.response.status === 403) {
-        // Si el error es por falta de permisos, mostrar un alert y redirigir
-        if (error.response.data.error === 'Solo los promotores o administradores pueden realizar reservas.') {
-            const confirmRedirect = window.confirm(
-                'No tienes permisos para realizar reservas. ¿Quieres convertirte en promotor?'
-            );
-            if (confirmRedirect) {
-                window.location.href = '/convertir-promotor';
-            }
-        }
-    } else {
-        console.error('Error al crear la reserva:', error);
-        alert('Hubo un error al crear la reserva. Inténtalo de nuevo.');
+      console.error('Error al crear la reserva:', error);
+      alert('Hubo un error al crear la reserva. Inténtalo de nuevo.');
     }
-}
-};
+  };
 
   return (
     <div>
       <h2>Sala de Celebraciones</h2>
       <div className="calendar-container">
-      <Calendar
-  onChange={handleDateChange}
-  value={selectedDate}
-  minDate={new Date()} // Evita que se seleccionen fechas pasadas
-  className="w-[380px] p-4 bg-[#e5cc70] rounded-lg shadow-lg border border-gray-300"
-  tileClassName={({ date }) => {
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
+        <Calendar
+          onChange={handleDateChange}
+          value={selectedDate}
+          minDate={new Date()} // Evita que se seleccionen fechas pasadas
+          className="w-[380px] p-4 bg-[#e5cc70] rounded-lg shadow-lg border border-gray-300"
+          tileClassName={({ date }) => {
+            const today = new Date();
+            today.setHours(0, 0, 0, 0);
 
-    if (date < today) {
-      return 'text-gray-400 pointer-events-none'; // Días pasados en gris y no seleccionables
-    }
-    if (isDateBooked(date)) {
-      return 'bg-[#860303] text-white font-semibold rounded-md'; // Fechas ocupadas en rojo oscuro
-    }
-    if (selectedDate && date.toDateString() === selectedDate.toDateString()) {
-      return 'bg-black text-white font-semibold rounded-md'; // Fecha seleccionada en negro
-    }
-    return 'hover:bg-gray-200 rounded-md'; // Hover en días normales
-  }}
-/>
-
-
+            if (date < today) {
+              return 'text-gray-400 pointer-events-none'; // Días pasados en gris y no seleccionables
+            }
+            if (isDateBooked(date)) {
+              return 'bg-[#860303] text-white font-semibold rounded-md'; // Fechas ocupadas en rojo oscuro
+            }
+            if (selectedDate && date.toDateString() === selectedDate.toDateString()) {
+              return 'bg-black text-white font-semibold rounded-md'; // Fecha seleccionada en negro
+            }
+            return 'hover:bg-gray-200 rounded-md'; // Hover en días normales
+          }}
+        />
       </div>
 
       <form onSubmit={handleSubmit} className="event-form">
@@ -177,75 +183,59 @@ const EventosSalaCelebraciones = () => {
             onChange={(e) => setNumeroPersonas(Number(e.target.value))}
             className="event-select"
           >
-            {[...Array(6)].map((_, index) => (
-              <option key={index} value={(index + 1) * 50}>
-                {(index + 1) * 50}
+            {[...Array(5)].map((_, index) => (
+              <option key={index} value={(index + 1) * 40}>
+                {(index + 1) * 40}
               </option>
             ))}
           </select>
         </label>
-
         <label>
-          <h3>Tipo de reserva:</h3>
-          <select
-            value={tipoReserva}
-            onChange={(e) => setTipoReserva(e.target.value)}
+          <h3>Nombre del concierto:</h3>
+          <input
+            type="text"
+            value={nombreConcierto}
+            onChange={(e) => setNombreConcierto(e.target.value)}
+            placeholder="Nombre del concierto"
             required
-          >
-            <option value="privada">Privada</option>
-            <option value="concierto">Concierto</option>
-          </select>
+          />
         </label>
-
-        {tipoReserva === 'concierto' && (
-          <>
-            <label>
-              <h3>Nombre del concierto:</h3>
-              <input
-                type="text"
-                value={nombreConcierto}
-                onChange={(e) => setNombreConcierto(e.target.value)}
-                placeholder="Nombre del concierto"
-                required
-              />
-            </label>
-            <label>
-              <h3>Hora de inicio:</h3>
-              <input
-                type="time"
-                value={horaInicio}
-                onChange={(e) => setHoraInicio(e.target.value)}
-                required
-              />
-            </label>
-            <label>
-              <h3>Hora de fin:</h3>
-              <input
-                type="time"
-                value={horaFin}
-                onChange={(e) => setHoraFin(e.target.value)}
-                required
-              />
-            </label>
-            <label>
-              <h3>Cartel del concierto:</h3>
-              <input type="file" onChange={handleCartelChange} accept="image/*" required />
-            </label>
-            <label>
-              <h3>Precio de entrada (€):</h3>
-              <input type="number" value={precioEntrada} onChange={(e) => setPrecioEntrada(e.target.value)} min="0" step="0.01" required />
-            </label>
-            <label>
-            <textarea
+        <label>
+          <h3>Hora de inicio:</h3>
+          <input
+            type="time"
+            value={horaInicio}
+            onChange={(e) => setHoraInicio(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <h3>Hora de fin:</h3>
+          <input
+            type="time"
+            value={horaFin}
+            onChange={(e) => setHoraFin(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <h3>Cartel del concierto:</h3>
+          <input type="file" onChange={handleCartelChange} accept="image/*" required />
+        </label>
+        <label>
+          <h3>Precio de entrada (€):</h3>
+          <input type="number" value={precioEntrada} onChange={(e) => setPrecioEntrada(e.target.value)} min="0" step="0.01" required />
+        </label>
+        <label>
+          <h3>Describa el evento:</h3>
+          <textarea
             value={motivo}
             onChange={(e) => setMotivo(e.target.value)}
             placeholder="Describa el evento o motivo"
             required
             className="event-textarea"
           />
-            </label>
-          </>
-        )}
+        </label>
 
         <label className="accept-policies">
           <input type="checkbox" checked={acceptPolicies} onChange={(e) => setAcceptPolicies(e.target.checked)} />
