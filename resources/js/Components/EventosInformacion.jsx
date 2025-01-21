99e813f1028ba350f1f5ce7781a3c0f849f9a813
victormoreiraruiz <victormanuel.moreira@iesdonana.org>
2025-01-21 21:04:54 +0100

    admin tambien crea reservas

--- resources/js/Components/EventosInformacion.jsx
+++ resources/js/Components/EventosInformacion.jsx
@@ -3,17 +3,26 @@ import React from 'react';
 const EventosInformacion = () => {
   return (
     <div className="informacion-container">
+      {/* Imagen recortada */}
       <div className="header-section">
-        <img src="/imagenes/EventoInformacion.png" alt="Evento Principal" className="header-image" />
+        <img
+          src="/imagenes/EventoInformacion.png"
+          alt="Evento Principal"
+          className="header-image w-full h-64 object-cover object-center"
+        />
       </div>
 
-      <div className="intro-section">
-        <h4>THE CODE EVENTOS</h4>
-        <br></br>
-        <h5>
-          The Code te permite la posibilidad de crear tu propio evento en la posiblemente mejor sala de Sanlúcar de Barrameda.<br></br>
-          Organiza tu evento en el mismo lugar donde han actuado los mejores artistas de la última década.<br></br>
-          Pero esto no es el pasado, ve hacia nuestro futuro.<br></br>
+      {/* Texto introductorio */}
+      <div className="intro-section px-4 py-6 text-center">
+        <h4 className="text-xl font-bold">THE CODE EVENTOS</h4>
+        <br />
+        <h5 className="text-base leading-7">
+          The Code te permite la posibilidad de crear tu propio evento en la posiblemente mejor sala de Sanlúcar de Barrameda.
+          <br />
+          Organiza tu evento en el mismo lugar donde han actuado los mejores artistas de la última década.
+          <br />
+          Pero esto no es el pasado, ve hacia nuestro futuro.
+          <br />
           Bienvenidos a sala The Code Eventos. Estás en casa.
         </h5>
       </div>
